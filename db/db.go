9ccb0c6d51fb1132ca6750d568a82f0f558e3303
Alessandro <alessandro2ap3@gmail.com>
2025-01-15 14:10:17 +0100

    added getEventById

--- db/db.go
+++ db/db.go
@@ -2,28 +2,38 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
+	"log"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/glebarez/sqlite" // Driver SQLite
 )
 
 var DB *sql.DB
 
+// InitDB inizializza la connessione al database SQLite.
 func InitDB() {
 	var err error
-	DB, err = sql.Open("sqlite3", "api.db")
-
+	// Connessione al database SQLite
+	DB, err = sql.Open("sqlite", "api.db")
 	if err != nil {
-		panic("Could not connect to database.")
+		log.Fatalf("Error opening database: %v", err)
 	}
 
+	// Verifica che il database sia raggiungibile
+	if err := DB.Ping(); err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+
+	// Impostazione delle connessioni
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
 
+	// Creazione delle tabelle
 	createTables()
 }
 
+// createTables crea le tabelle necessarie nel database SQLite.
 func createTables() {
+	// Query per la creazione della tabella "events"
 	createEventsTable := `
     CREATE TABLE IF NOT EXISTS events (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,15 +41,15 @@ func createTables() {
       description TEXT NOT NULL,
       location TEXT NOT NULL,
       dateTime DATETIME NOT NULL,
-      user_id INTEGER,
+      user_id INTEGER
     )
     `
 
+	// Esegui la query
 	_, err := DB.Exec(createEventsTable)
-
 	if err != nil {
-		fmt.Println(err)
-		panic("Could not create events table.")
+		log.Fatalf("Error creating events table: %v", err)
 	}
 
+	log.Println("Events table created or already exists.")
 }
