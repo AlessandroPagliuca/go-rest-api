cb466a8d7ed9a385c34175556bfe95e4d782e9bf
Alessandro <alessandro2ap3@gmail.com>
2025-01-15 18:33:50 +0100

    added jwt

---
+++ utils/jwt.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const secretKey string = "supersecret"
+
+func GenerateToken(email string, userId int64) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"email":  email,
+		"userId": userId,
+		"exp":    time.Now().Add(time.Hour * 2).Unix(),
+	})
+
+	return token.SignedString(secretKey)
+
+}
